a166d51fa17e3e73a5afecb0f3257feb788ba312
dhivek1 <dhivek09@gmail.com>
2025-06-03 22:53:25 +0900

    Update menuItems.ts

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -2,15 +2,7 @@ import { IMenuItem } from "@/types";
 
 export const menuItems: IMenuItem[] = [
     {
-        text: "Features",
-        url: "#features"
-    },
-    {
-        text: "Pricing",
-        url: "#pricing"
-    },
-    {
-        text: "Testimonials",
-        url: "#testimonials"
-    }
+        text: "Emission Calculator",
+        url: "#calculator"
+   
 ];

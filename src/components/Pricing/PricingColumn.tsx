2d193d8ddb57c4166f5e18d35dfaaff2bc358459
dhivek1 <dhivek09@gmail.com>
2025-06-03 22:59:29 +0900

    Update PricingColumn.tsx

--- src/components/Pricing/PricingColumn.tsx
+++ src/components/Pricing/PricingColumn.tsx
@@ -9,25 +9,32 @@ interface Props {
 }
 
 const PricingColumn: React.FC<Props> = ({ tier, highlight }: Props) => {
-    const { name, price, features } = tier;
+    const { name, features } = tier;
 
     return (
-        <div className={clsx("w-full max-w-sm mx-auto bg-white rounded-xl border border-gray-200 lg:max-w-full", { "shadow-lg": highlight })}>
+        <div className={clsx("w-full max-w-sm mx-auto bg-white rounded-xl border border-gray-200 lg:max-w-full", {
+            "shadow-lg": highlight
+        })}>
             <div className="p-6 border-b border-gray-200 rounded-t-xl">
                 <h3 className="text-2xl font-semibold mb-4">{name}</h3>
-                <p className="text-3xl md:text-5xl font-bold mb-6">
-                    <span className={clsx({ "text-secondary": highlight })}>
-                        {typeof price === 'number' ? `$${price}` : price}
-                    </span>
-                    {typeof price === 'number' && <span className="text-lg font-normal text-gray-600">/mo</span>}
+                <p className="text-lg text-gray-700 mb-6">
+                    Get AI-powered carbon footprint reports tailored to your company’s size, industry, and compliance needs.
                 </p>
-                <button className={clsx("w-full py-3 px-4 rounded-full transition-colors", { "bg-primary hover:bg-primary-accent": highlight, "bg-hero-background hover:bg-gray-200": !highlight })}>
-                    Get Started
+                <button
+                    className={clsx(
+                        "w-full py-3 px-4 rounded-full transition-colors",
+                        {
+                            "bg-primary hover:bg-primary-accent text-white": highlight,
+                            "bg-hero-background hover:bg-gray-200 text-black": !highlight
+                        }
+                    )}
+                >
+                    Request Demo
                 </button>
             </div>
             <div className="p-6 mt-1">
                 <p className="font-bold mb-0">FEATURES</p>
-                <p className="text-foreground-accent mb-5">Everything in basic, plus...</p>
+                <p className="text-foreground-accent mb-5">What you’ll get with this plan:</p>
                 <ul className="space-y-4 mb-8">
                     {features.map((feature, index) => (
                         <li key={index} className="flex items-center">
@@ -38,7 +45,7 @@ const PricingColumn: React.FC<Props> = ({ tier, highlight }: Props) => {
                 </ul>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PricingColumn
+export default PricingColumn;

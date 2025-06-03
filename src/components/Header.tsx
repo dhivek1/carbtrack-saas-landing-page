ad388f32c44e4ec1a81220d9fa8b456bcced4524
dhivek1 <dhivek09@gmail.com>
2025-06-03 22:00:40 +0900

    chaanges

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { HiOutlineXMark, HiBars3 } from 'react-icons/hi2';
-import { FaFingerprint } from 'react-icons/fa';
+import { GiFootprint } from "react-icons/gi";
 
 import Container from './Container';
 import { menuItems } from '@/data/menuItems';
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
                 <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
                     {/* Logo */}
                     <Link href="/" className="flex items-center gap-2">
-                        <FaFingerprint className="text-foreground min-w-fit w-7 h-7" />
+                        <GiFootprint className="text-foreground min-w-fit w-7 h-7" />
                         <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
                             Grio.ai
                         </span>
@@ -38,8 +38,11 @@ const Header: React.FC = () => {
                             </li>
                         ))}
                         <li>
-                            <Link href="/calculator" className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors">
-                                Emission Tool
+                            <Link
+                                href="/auth"
+                                className="text-white bg-green-600 hover:bg-green-700 px-8 py-3 rounded-full transition-colors"
+                            >
+                                Login
                             </Link>
                         </li>
                     </ul>
@@ -84,8 +87,12 @@ const Header: React.FC = () => {
                             </li>
                         ))}
                         <li>
-                            <Link href="/calculator" className="text-black bg-primary hover:bg-primary-accent px-5 py-2 rounded-full block w-fit" onClick={toggleMenu}>
-                                Emission Tool
+                            <Link
+                                href="/auth"
+                                className="text-white bg-green-600 hover:bg-green-700 px-5 py-2 rounded-full block w-fit"
+                                onClick={toggleMenu}
+                            >
+                                Login
                             </Link>
                         </li>
                     </ul>

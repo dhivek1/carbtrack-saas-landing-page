cbf0fb7d9eae0079c2bf84de60ffff493d4a1e4a
dhivek1 <dhivek09@gmail.com>
2025-06-04 22:24:42 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import FAQ from "@/components/FAQ";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
 import Section from "@/components/Section";
-import Stats from "@/components/Stats";
-import CTA from "@/components/CTA";
 
 const HomePage: React.FC = () => {
   return (
